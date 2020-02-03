7c5bf30ae224469f109bf9a4cc205da5ada91221
YKStudio <ashn01@naver.com>
2020-02-02 19:00:59 -0500

    First commit

---
+++ Gomoku/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class Form1 : Form
+    {
+        const int margin = 40;
+        const int gridSize = 30;
+        const int stoneSize = 27;
+        const int dotSize = 10;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+    }
+}
