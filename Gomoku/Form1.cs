3ab61235c9c67ed2100acc6f57848347faf61986
YKStudio <ashn01@naver.com>
2020-02-03 14:43:03 -0500

    Implemented Putting stones

--- Gomoku/Form1.cs
+++ Gomoku/Form1.cs
@@ -12,16 +12,19 @@ namespace Gomoku
 {
     public partial class Form1 : Form
     {
+        const int FIELD = 19;
         const int margin = 40;
         const int gridSize = 30;
         const int stoneSize = 27;
         const int dotSize = 10;
-
+        int[,] gomokuField = new int[19,19];
+        int turn = 0;
 
         public Form1()
         {
             InitializeComponent();
             Field.Paint += new PaintEventHandler(DrawingField);
+            Field.MouseDown += new MouseEventHandler(PutStone);
             ResizeRedraw = true;
         }
 
@@ -33,7 +36,7 @@ namespace Gomoku
         private void DrawingField(object sender, PaintEventArgs e)
         {
             Pen p = new Pen(Color.Black, 1);
-            for(int i=0;i<19;++i)
+            for(int i=0;i< FIELD; ++i)
             {
                 e.Graphics.DrawLine(p, margin, margin+gridSize*i, gridSize*18+margin, margin+ gridSize * i);
                 e.Graphics.DrawLine(p, margin+ gridSize * i, margin, margin+ gridSize * i, gridSize * 18 + margin);
@@ -43,9 +46,40 @@ namespace Gomoku
                     e.Graphics.FillEllipse(Brushes.Black, i*gridSize*6 + margin + gridSize * 3 - dotSize / 2, j * gridSize * 6 + margin + gridSize * 3 - dotSize / 2, dotSize, dotSize);
         }
 
-        private void newGameMenu_Click(object sender, EventArgs e)
+        private void PutStone(object sender, MouseEventArgs e)
         {
-            this.Field.Invalidate();
+            Calculate(new Point(e.X - margin, e.Y - margin));
         }
+
+        private bool Calculate(Point p) // position
+        {
+            p.X += 7;
+            p.Y += 7;
+            if (p.X <= 0 || p.Y <= 0 || p.X >= 19 * gridSize || p.Y >= 19 * gridSize)
+                return false;
+            if (p.X % gridSize > gridSize / 2 || p.Y % gridSize > gridSize / 2)
+                return false;
+
+            p.X = p.X / gridSize + (p.X % gridSize > gridSize / 2 ? 1 : 0);
+            p.Y = p.Y / gridSize + (p.Y % gridSize > gridSize / 2 ? 1 : 0);
+
+            if (gomokuField[p.X, p.Y] != 1)
+            { // draw stone
+                gomokuField[p.X, p.Y] = 1;
+                ++turn;
+                Graphics g = this.Field.CreateGraphics();
+                g.FillEllipse((turn%2 == 0 ? Brushes.Black : Brushes.White), 
+                    margin - stoneSize / 2 + p.X * gridSize, 
+                    margin - stoneSize / 2 + p.Y * gridSize, 
+                    stoneSize, 
+                    stoneSize);
+                
+                return true;
+            }
+            else
+                return false;
+        }
+
+
     }
 }
