f74627c83f9ea449b267d7eab67830808143c86d
YKStudio <ashn01@naver.com>
2020-02-03 17:46:08 -0500

    Implemented gomoku game

--- Gomoku/Form1.Designer.cs
+++ Gomoku/Form1.Designer.cs
@@ -29,27 +29,108 @@
         private void InitializeComponent()
         {
             this.Field = new System.Windows.Forms.Panel();
+            this.lTitle = new System.Windows.Forms.Label();
+            this.lBlack = new System.Windows.Forms.Label();
+            this.lWhite = new System.Windows.Forms.Label();
+            this.lBlackScore = new System.Windows.Forms.Label();
+            this.lWhiteScore = new System.Windows.Forms.Label();
+            this.lTurn = new System.Windows.Forms.Label();
+            this.bRestart = new System.Windows.Forms.Button();
+            this.Field.SuspendLayout();
             this.SuspendLayout();
             // 
             // Field
             // 
             this.Field.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(51)))));
+            this.Field.Controls.Add(this.bRestart);
+            this.Field.Controls.Add(this.lTurn);
+            this.Field.Controls.Add(this.lWhiteScore);
+            this.Field.Controls.Add(this.lBlackScore);
+            this.Field.Controls.Add(this.lWhite);
+            this.Field.Controls.Add(this.lBlack);
+            this.Field.Controls.Add(this.lTitle);
             this.Field.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Field.Location = new System.Drawing.Point(0, 0);
             this.Field.Name = "Field";
-            this.Field.Size = new System.Drawing.Size(624, 621);
+            this.Field.Size = new System.Drawing.Size(734, 621);
             this.Field.TabIndex = 0;
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.lTitle.Location = new System.Drawing.Point(610, 40);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(89, 22);
+            this.lTitle.TabIndex = 0;
+            this.lTitle.Text = "Gomoku";
+            // 
+            // lBlack
+            // 
+            this.lBlack.AutoSize = true;
+            this.lBlack.Location = new System.Drawing.Point(622, 104);
+            this.lBlack.Name = "lBlack";
+            this.lBlack.Size = new System.Drawing.Size(36, 12);
+            this.lBlack.TabIndex = 1;
+            this.lBlack.Text = "Black";
+            // 
+            // lWhite
+            // 
+            this.lWhite.AutoSize = true;
+            this.lWhite.Location = new System.Drawing.Point(622, 135);
+            this.lWhite.Name = "lWhite";
+            this.lWhite.Size = new System.Drawing.Size(35, 12);
+            this.lWhite.TabIndex = 2;
+            this.lWhite.Text = "White";
+            // 
+            // lBlackScore
+            // 
+            this.lBlackScore.AutoSize = true;
+            this.lBlackScore.Location = new System.Drawing.Point(674, 104);
+            this.lBlackScore.Name = "lBlackScore";
+            this.lBlackScore.Size = new System.Drawing.Size(11, 12);
+            this.lBlackScore.TabIndex = 3;
+            this.lBlackScore.Text = "0";
+            // 
+            // lWhiteScore
+            // 
+            this.lWhiteScore.AutoSize = true;
+            this.lWhiteScore.Location = new System.Drawing.Point(674, 135);
+            this.lWhiteScore.Name = "lWhiteScore";
+            this.lWhiteScore.Size = new System.Drawing.Size(11, 12);
+            this.lWhiteScore.TabIndex = 4;
+            this.lWhiteScore.Text = "0";
+            // 
+            // lTurn
+            // 
+            this.lTurn.AutoSize = true;
+            this.lTurn.Location = new System.Drawing.Point(612, 73);
+            this.lTurn.Name = "lTurn";
+            this.lTurn.Size = new System.Drawing.Size(0, 12);
+            this.lTurn.TabIndex = 5;
+            // 
+            // bRestart
+            // 
+            this.bRestart.Location = new System.Drawing.Point(614, 174);
+            this.bRestart.Name = "bRestart";
+            this.bRestart.Size = new System.Drawing.Size(85, 23);
+            this.bRestart.TabIndex = 6;
+            this.bRestart.Text = "Reset";
+            this.bRestart.UseVisualStyleBackColor = true;
+            this.bRestart.Click += new System.EventHandler(this.bRestart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 621);
+            this.ClientSize = new System.Drawing.Size(734, 621);
             this.Controls.Add(this.Field);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Gomoku";
+            this.Field.ResumeLayout(false);
+            this.Field.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +138,13 @@
         #endregion
 
         private System.Windows.Forms.Panel Field;
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Label lWhite;
+        private System.Windows.Forms.Label lBlack;
+        private System.Windows.Forms.Label lTurn;
+        private System.Windows.Forms.Label lWhiteScore;
+        private System.Windows.Forms.Label lBlackScore;
+        private System.Windows.Forms.Button bRestart;
     }
 }
 
--- Gomoku/Form1.cs
+++ Gomoku/Form1.cs
@@ -12,20 +12,33 @@ namespace Gomoku
 {
     public partial class Form1 : Form
     {
+        enum Player{ BLACK=0,WHITE=1 }
         const int FIELD = 19;
         const int margin = 40;
         const int gridSize = 30;
         const int stoneSize = 27;
         const int dotSize = 10;
-        int[,] gomokuField = new int[19,19];
+        int[,] gomokuField = new int[19, 19];
         int turn = 0;
+        bool gameSet = false;
 
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            Init();
+        }
+
+        private void Init()
+        {
+            this.Field.Invalidate();
+            Array.Clear(gomokuField, 0, gomokuField.Length);
+            turn = 0;
+            gameSet = false;
             Field.Paint += new PaintEventHandler(DrawingField);
             Field.MouseDown += new MouseEventHandler(PutStone);
             ResizeRedraw = true;
+
+            lTurn.Text = (turn % 2 == 0 ? "Black" : "White") + " turn";
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -36,19 +49,38 @@ namespace Gomoku
         private void DrawingField(object sender, PaintEventArgs e)
         {
             Pen p = new Pen(Color.Black, 1);
-            for(int i=0;i< FIELD; ++i)
+            for (int i = 0; i < FIELD; ++i)
             {
-                e.Graphics.DrawLine(p, margin, margin+gridSize*i, gridSize*18+margin, margin+ gridSize * i);
-                e.Graphics.DrawLine(p, margin+ gridSize * i, margin, margin+ gridSize * i, gridSize * 18 + margin);
+                e.Graphics.DrawLine(p, margin, margin + gridSize * i, gridSize * 18 + margin, margin + gridSize * i);
+                e.Graphics.DrawLine(p, margin + gridSize * i, margin, margin + gridSize * i, gridSize * 18 + margin);
             }
-            for(int i=0;i<3;++i)
-                for(int j=0;j<3;++j)
-                    e.Graphics.FillEllipse(Brushes.Black, i*gridSize*6 + margin + gridSize * 3 - dotSize / 2, j * gridSize * 6 + margin + gridSize * 3 - dotSize / 2, dotSize, dotSize);
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 3; ++j)
+                    e.Graphics.FillEllipse(Brushes.Black, i * gridSize * 6 + margin + gridSize * 3 - dotSize / 2, j * gridSize * 6 + margin + gridSize * 3 - dotSize / 2, dotSize, dotSize);
         }
 
         private void PutStone(object sender, MouseEventArgs e)
         {
-            Calculate(new Point(e.X - margin, e.Y - margin));
+            if (gameSet) // stop game if anyone wins.
+                return;
+
+            Point p = new Point(e.X - margin, e.Y - margin);
+            if (Calculate(p))
+            {
+                if (CheckWin(p))
+                {
+                    int who = WhosTurn();
+                    if (who == (int)Player.BLACK)
+                        lBlackScore.Text = (Convert.ToInt32(lBlackScore.Text)+1).ToString();
+                    else
+                        lWhiteScore.Text = (Convert.ToInt32(lWhiteScore.Text + 1)+1).ToString();
+
+                    lTurn.Text = (who == (int)Player.BLACK ? "Black" : "White") + " Win!!";
+
+                    gameSet = true; // game over
+                }
+                ++turn;
+            }
         }
 
         private bool Calculate(Point p) // position
@@ -63,23 +95,120 @@ namespace Gomoku
             p.X = p.X / gridSize + (p.X % gridSize > gridSize / 2 ? 1 : 0);
             p.Y = p.Y / gridSize + (p.Y % gridSize > gridSize / 2 ? 1 : 0);
 
-            if (gomokuField[p.X, p.Y] != 1)
+            if (gomokuField[p.X, p.Y] == 0)
             { // draw stone
-                gomokuField[p.X, p.Y] = 1;
-                ++turn;
+                gomokuField[p.X, p.Y] = turn % 2 == 0 ? 1 : 2; // 1 black, 2 white
                 Graphics g = this.Field.CreateGraphics();
-                g.FillEllipse((turn%2 == 0 ? Brushes.Black : Brushes.White), 
-                    margin - stoneSize / 2 + p.X * gridSize, 
-                    margin - stoneSize / 2 + p.Y * gridSize, 
-                    stoneSize, 
+                g.FillEllipse((turn % 2 == 0 ? Brushes.Black : Brushes.White),
+                    margin - stoneSize / 2 + p.X * gridSize,
+                    margin - stoneSize / 2 + p.Y * gridSize,
+                    stoneSize,
                     stoneSize);
-                
+
+
                 return true;
             }
             else
                 return false;
         }
 
+        private bool CheckWin(Point p) // check if any player wins.
+        {
+            int cntVertical = 0, cntHorizontal = 0, cntDiagonalNE = 0, cntDiagonalSE = 0;
+            p.X = p.X / gridSize + (p.X % gridSize > gridSize / 2 ? 1 : 0);
+            p.Y = p.Y / gridSize + (p.Y % gridSize > gridSize / 2 ? 1 : 0);
+            int who = WhosTurn()+1;
+            // check vertically
+            for (int i = 0; p.Y >= 0; i++)
+            {
+                if (gomokuField[p.X, p.Y - i] == who)
+                    ++cntVertical;
+                else
+                    break;
+            }
 
+            for (int i = 1; p.Y <= FIELD; i++)
+            {
+                if (gomokuField[p.X, p.Y + i] == who)
+                    ++cntVertical;
+                else
+                    break;
+            }
+            if (cntVertical == 5)
+                return true;
+
+            // check horizontally
+            for (int i = 0; p.X >= 0; i++)
+            {
+                if (gomokuField[p.X - i, p.Y] == who)
+                    ++cntHorizontal;
+                else
+                    break;
+            }
+
+            for (int i = 1; p.Y <= FIELD; i++)
+            {
+                if (gomokuField[p.X + i, p.Y] == who)
+                    ++cntHorizontal;
+                else
+                    break;
+            }
+
+            if (cntHorizontal == 5)
+                return true;
+
+            // check diagonal NE
+            for (int i = 0; p.Y >= 0 && p.X <= FIELD; i++)
+            {
+                if (gomokuField[p.X + i, p.Y - i] == who)
+                    ++cntDiagonalNE;
+                else
+                    break;
+            }
+
+            for (int i = 1; p.Y <= FIELD && p.X >= 0; i++)
+            {
+                if (gomokuField[p.X - i, p.Y + i] == who)
+                    ++cntDiagonalNE;
+                else
+                    break;
+            }
+            if (cntDiagonalNE == 5)
+                return true;
+
+            // check diagonal SE
+            for (int i = 0; p.Y >= 0 && p.X >= 0; i++)
+            {
+                if (gomokuField[p.X - i, p.Y - i] == who)
+                    ++cntDiagonalSE;
+                else
+                    break;
+            }
+
+            for (int i = 1; p.Y <= FIELD && p.X <= FIELD; i++)
+            {
+                if (gomokuField[p.X + i, p.Y + i] == who)
+                    ++cntDiagonalSE;
+                else
+                    break;
+            }
+            if (cntDiagonalSE == 5)
+                return true;
+
+            return false;
+        }
+
+        private int WhosTurn()
+        {
+            int who = turn % 2 ;
+            lTurn.Text = (who != (int)Player.BLACK ? "Black" : "White") + " turn";
+
+            return who;
+        }
+
+        private void bRestart_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
     }
 }
